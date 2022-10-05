33b0a176e1d6edc37ff4c09086f8e1852a523e2d
Benjamin Plate <bplate@berkeley.edu>
2022-10-05 21:21:21 +0000

    define user_file_list_destroy

--- src/userprog/userfile.h
+++ src/userprog/userfile.h
@@ -14,5 +14,6 @@ typedef struct list user_file_list;
 int user_file_open(user_file_list* list, const char* file, int fd);
 void user_file_close(user_file_list* list, int fd);
 struct user_file* user_file_get(user_file_list* list, int fd);
+void user_file_list_destroy(user_file_list* list);
 
 #endif /* userprog/userfile.h */
