3655a98005e1f85ecdaa8b4c25a57d467ec4b045
Teddy Cho <teddycho@berkeley.edu>
2022-10-11 04:57:03 +0000

    finished testing

---
+++ src/tests/userprog/my-test-1.c
@@ -0,0 +1,10 @@
+/* Invokes a system call with an invalid system call number.
+   The process must be terminated with -1 exit code. */
+
+#include "tests/lib.h"
+#include "tests/main.h"
+
+void test_main(void) {
+  asm volatile("pushl $0x20; int $0x30");
+  fail("should have killed process");
+}
