f94262c0f2679c737f975b2b85d7b3f8f60cbd72
Jeffrey Liang <jeffreyliang@berkeley.edu>
2022-11-27 01:03:38 +0000

    Implement helper functions cache_get_entry and cache_release_entry

--- src/filesys/cache.c
+++ src/filesys/cache.c
@@ -1,5 +1,7 @@
 #include "filesys/cache.h"
 #include <debug.h>
+#include "devices/timer.h"
+#include "filesys/filesys.h"
 #include "threads/synch.h"
 
 /* Number of entries in the cache. */
@@ -12,8 +14,8 @@ struct cache_entry {
   bool valid;                      /* True if data valid. */
   bool dirty;                      /* True if data should be written back on eviction. */
   uint64_t last_accessed;          /* Ticks of last access. */
-  int num_accessing;               /* Number of threads trying to access data. */
-  struct condition valid_wait;     /* Wait for valid data. */
+  int num_accessing;               /* Number of threads trying to access. Do not evict if >0. */
+  struct condition valid_wait;     /* Wait for valid data. Used with cache_lock. */
   struct lock data_lock;           /* Lock on data. */
   uint8_t data[BLOCK_SECTOR_SIZE]; /* Data on disk. */
 };
@@ -34,6 +36,88 @@ void cache_init(void) {
     lock_init(&buffer_cache[i].data_lock);
   }
   lock_init(&cache_lock);
+}
+
+/* Returns a pointer to an entry in buffer_cache matching SECTOR
+   or the entry to evict based on LRU.
+   Set READ_ON_MISS to true if the data in SECTOR should be read
+   into the entry on a cache miss (i.e., READ_ON_MISS can be set
+   to false on a blind write). */
+static struct cache_entry* cache_get_entry(block_sector_t sector, bool read_on_miss) {
+  struct cache_entry* entry = NULL;
+  lock_acquire(&cache_lock);
+
+  for (int i = 0; i < CACHE_SIZE; i++) {
+    /* Cache hit */
+    if (buffer_cache[i].sector == sector) {
+      buffer_cache[i].num_accessing += 1;
+      if (!buffer_cache[i].valid) {
+        cond_wait(&buffer_cache[i].valid_wait, &cache_lock);
+      }
+      entry = &buffer_cache[i];
+      break;
+    }
+    /* No hit yet, track entry to evict by LRU */
+    if (buffer_cache[i].num_accessing == 0 &&
+        (entry == NULL || buffer_cache[i].last_accessed < entry->last_accessed)) {
+      entry = &buffer_cache[i];
+    }
+  }
+
+  /* On miss, while holding cache_lock, invalidate the entry */
+  block_sector_t old_sector = entry->sector;
+  if (old_sector != sector) {
+    entry->sector = sector;
+    entry->valid = false;
+    entry->num_accessing += 1;
+  }
+
+  ASSERT(entry->num_accessing >= 1);
+
+  lock_release(&cache_lock);
+
+  if (old_sector != sector) {
+    /* On miss, while holding data_lock, write back data if dirty
+       and read in data if specified */
+    lock_acquire(&entry->data_lock);
+    if (entry->dirty) {
+      block_write(fs_device, old_sector, entry->data);
+    }
+    if (read_on_miss) {
+      block_read(fs_device, sector, entry->data);
+    }
+    lock_release(&entry->data_lock);
+
+    /* On miss, while holding cache_lock, update entry's metadata
+       and validate data if new data was read in */
+    lock_acquire(&cache_lock);
+    entry->dirty = false;
+    if (read_on_miss) {
+      entry->valid = true;
+      cond_broadcast(&entry->valid_wait, &cache_lock);
+    }
+    lock_release(&cache_lock);
+  }
+
+  return entry;
+}
+
+/* Releases ENTRY to be used by another thread.
+   Set VALID to true if the data in ENTRY is valid
+   and set DIRTY to true if this entry was written to. */
+static void cache_release_entry(struct cache_entry* entry, bool valid, bool dirty) {
+  lock_acquire(&cache_lock);
+  ASSERT(entry->num_accessing >= 1);
+
+  if (!entry->valid && valid) {
+    cond_broadcast(&entry->valid_wait, &cache_lock);
+  }
+  entry->valid = valid;
+  entry->dirty = dirty;
+  entry->last_accessed = timer_ticks();
+  entry->num_accessing -= 1;
+
+  lock_release(&cache_lock);
 }
 
 /* Reads the contents of SECTOR into BUFFER via the cache. */
