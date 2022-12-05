68f7a4c4ec30f85fc95b9e7ab82a8ac177f42400
Benjamin Plate <bplate@berkeley.edu>
2022-12-05 19:55:55 +0000

    implement user_dir equivalents

--- src/userprog/userfile.h
+++ src/userprog/userfile.h
@@ -16,10 +16,16 @@ struct user_dir {
 };
 
 typedef struct list user_file_list;
+typedef struct list user_dir_list;
 
 int user_file_open(user_file_list* list, const char* file, int fd);
 void user_file_close(user_file_list* list, int fd);
 struct user_file* user_file_get(user_file_list* list, int fd);
 void user_file_list_destroy(user_file_list* list);
+
+int user_dir_open(user_dir_list* list, const char* dir, int fd);
+void user_dir_close(user_dir_list* list, int fd);
+struct user_dir* user_dir_get(user_dir_list* list, int fd);
+void user_dir_list_destroy(user_dir_list* list);
 
 #endif /* userprog/userfile.h */
