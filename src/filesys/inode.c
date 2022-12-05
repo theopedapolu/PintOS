d905caff4273efcdadd80e85f2d851e1e8d17483
Benjamin Plate <bplate@berkeley.edu>
2022-12-05 19:12:15 +0000

    add is_dir to inode_disk

--- src/filesys/inode.c
+++ src/filesys/inode.c
@@ -31,6 +31,7 @@ static const off_t DOUBLY_INDIRECT_CAPACITY =
 /* On-disk inode.
    Must be exactly BLOCK_SECTOR_SIZE bytes long. */
 struct inode_disk {
+  bool is_dir;                                /* True if this inode represents a directory. */
   block_sector_t direct[NUM_DIRECT_POINTERS]; /* Direct pointers. */
   block_sector_t indirect;                    /* Indirect pointer. */
   block_sector_t doubly_indirect;             /* Doubly indirect pointer. */
@@ -258,10 +259,10 @@ void inode_init(void) {
 
 /* Initializes an inode with LENGTH bytes of data and
    writes the new inode to sector SECTOR on the file system
-   device.
+   device. Creates a directory if IS_DIR is true.
    Returns true if successful.
    Returns false if memory or disk allocation fails. */
-bool inode_create(block_sector_t sector, off_t length) {
+bool inode_create(block_sector_t sector, off_t length, bool is_dir) {
   ASSERT(length >= 0);
 
   struct inode_disk* disk_inode = cache_get_buffer(sector, false);
@@ -270,6 +271,7 @@ bool inode_create(block_sector_t sector, off_t length) {
      one sector in size, and you should fix that. */
   ASSERT(sizeof *disk_inode == BLOCK_SECTOR_SIZE);
 
+  disk_inode->is_dir = is_dir;
   disk_inode->length = length;
   disk_inode->magic = INODE_MAGIC;
   for (int i = 0; i < NUM_DIRECT_POINTERS; i++) {
